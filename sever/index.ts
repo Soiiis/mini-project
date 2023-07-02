6ab14fa887590a0327b52a6aa8ab8827374173f9
Nguyen Van Truong <truongpt02@gmail.com>
2023-07-02 21:53:34 +0700

    fix err

--- sever/index.ts
+++ sever/index.ts
@@ -39,6 +39,6 @@ app.use("/location", locationManagerRoute);
 app.use("/payment", paymentRecordRoute);
 app.use("/reward", rewardManagerRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
